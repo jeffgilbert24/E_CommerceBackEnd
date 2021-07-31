7135bcd2510ee33d373db697d2cf6195d47f9948
IBBear <jgilbert868@gmail.com>
2021-07-30 18:26:37 -0700

    update index file in model

--- models/index.js
+++ models/index.js
@@ -5,13 +5,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category,{
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'prod_many_tag'
+})
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_many_prod'
+})
 module.exports = {
   Product,
   Category,
