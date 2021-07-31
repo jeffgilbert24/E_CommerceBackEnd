0925f29cfafe38113709e5d818a08ff274340a13
IBBear <jgilbert868@gmail.com>
2021-07-31 13:18:02 -0700

    has bugs

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,7 +16,7 @@ router.get('/', async(req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try{
@@ -33,7 +33,7 @@ router.get('/:id', (req, res) => {
   } 
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
   try{
     const catIdValue = await Category.create({
@@ -45,7 +45,7 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a category by its `id` value
   try{
     const catIdValue = await Category.update({
@@ -66,7 +66,7 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete a category by its `id` value
   try{
     const catIdValue = await Category.destroy({ 
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try{
@@ -34,7 +34,7 @@ router.get('/:id', async (req, res) => {
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -66,7 +66,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -107,7 +107,7 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
 });
 
